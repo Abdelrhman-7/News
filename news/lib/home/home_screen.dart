abbb1fe2512067a3a4faef1b9bd4c4648b17b061
abdelrhman <wwwabdelrhmanmahmoud22@gmail.com>
2025-10-29 09:52:47 +0300

    statr work in home screen

--- news/lib/home/home_screen.dart
+++ news/lib/home/home_screen.dart
@@ -4,6 +4,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Column(children: [
+        Text(""),
+      ],)
+    );
   }
 }
