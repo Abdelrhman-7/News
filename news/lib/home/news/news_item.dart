eacba2d9306b00c885b9e2773303c646abfd8bb7
abdelrhman <wwwabdelrhmanmahmoud22@gmail.com>
2025-10-31 03:56:08 +0200

    1-creat function getSourseBySourseId in apimanager and handel cases in file NewsWidget 2- desiden news_Ui and get news from Api 3-change syntex in AppTheme

--- news/lib/home/news/news_item.dart
+++ news/lib/home/news/news_item.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:news/Utils/appTheme.dart';
 import 'package:news/core/colormanager.dart';
 import 'package:news/model/NewsResponse.dart';
 
